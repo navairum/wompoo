f01774d3c6ec6fa9f56efdfd9e2066d01f363049
Eric Intzandt <erk_3@hotmail.com>
2015-11-16 20:15:12 -0500

    -image renames -card layout initial.

--- Wompoo/app/src/main/java/wompoo/eric/com/wompoo/GameBoard.java
+++ Wompoo/app/src/main/java/wompoo/eric/com/wompoo/GameBoard.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class GameBoard extends ActionBarActivity {
@@ -12,6 +13,7 @@ public class GameBoard extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
+        //((ImageView)findViewById(R.id.whitecard)).setImageResource(R.drawable.clubs1);
     }
 
     @Override
